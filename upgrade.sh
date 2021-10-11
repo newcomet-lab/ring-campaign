d5cf371c8592e378fc2c9ec47a1efc485309ee05
newcomet-lab <newcometcto729@gmail.com>
2021-10-11 23:42:43 +0330

    fix idl typo

--- upgrade.sh
+++ upgrade.sh
@@ -2,5 +2,5 @@
 #solana airdrop 10
 anchor upgrade --program-id 2bFt7FK3K8DjjeJCXrv9fqhc34SPo2mWWMNqWgANJngJ ./target/deploy/datafarm.so
 anchor upgrade --program-id DQU8naysQoj3t9EEhDcFt8LUyGSTJrxMVXYFwviHshYF ./target/deploy/staking.so
-anchor idl upgrade --filepath ./target/idl/contracts.json 2bFt7FK3K8DjjeJCXrv9fqhc34SPo2mWWMNqWgANJngJ
-anchor idl upgrade --filepath ./target/idl/staking.json DQU8naysQoj3t9EEhDcFt8LUyGSTJrxMVXYFwviHshYF
+anchor idl upgrade --filepath ./target/idl/Datafarm.json 2bFt7FK3K8DjjeJCXrv9fqhc34SPo2mWWMNqWgANJngJ
+anchor idl upgrade --filepath ./target/idl/Staking.json DQU8naysQoj3t9EEhDcFt8LUyGSTJrxMVXYFwviHshYF
