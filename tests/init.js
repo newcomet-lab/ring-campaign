8b45a43a28a4102018640af02b338c5e80e64884
newcomet-lab <newcometcto729@gmail.com>
2022-01-10 12:11:15 +0330

    fix initializer with airdrop

--- tests/init.js
+++ tests/init.js
@@ -9,23 +9,29 @@ const {
     SystemProgram,
 } = require("@solana/web3.js");
 const {userCharge, ourCharge, vaultCharge,findAssociatedTokenAddress, hash} = require("./helper");
-const {TOKEN_PROGRAM_ID,Token} = require('@solana/spl-token');
+const splToken = require('@solana/spl-token');
 const {createTokenAccount, sleep} = require("@project-serum/common");
 const TokenInstructions = require("@project-serum/serum").TokenInstructions;
 
 describe('Initialize Data Yield Farm', () => {
     anchor.setProvider(anchor.Provider.env());
-
+    const provider = anchor.getProvider();
     const data_idl = JSON.parse(
         require('fs')
             .readFileSync('target/idl/Datafarm.json', 'utf8')
             .toString())
 
     const program = new anchor.Program(data_idl, data_idl.metadata.address, anchor.getProvider());
-    const provider = anchor.getProvider();
-    let admin ;
-    let mint  = new anchor.web3.PublicKey('BSNHwP8nRzvgurmn6FPxUQkWY2KYC64FGPMU1QKzxuiD');
-    let poolVault  =new anchor.web3.PublicKey('DEhtrRycYC2gZmv4z9WCD6qvPrtWdLD8dEHPY4qr856y');
+    const ks_pool = fs.readFileSync("/home/hadi/.config/solana/id.json", {encoding: 'utf8'});
+    const kb_pool = Buffer.from(JSON.parse(ks_pool));
+    let admin = new anchor.web3.Account(kb_pool);
+    let mint;
+
+    it("Create SNS token for test", async () => {
+        mint = await splToken.Token.createMint(provider.connection, admin, admin.publicKey, admin.publicKey, 9, splToken.TOKEN_PROGRAM_ID);
+        console.log('\ttest SNS Token address: ' + mint.publicKey.toBase58());
+    }).timeout(190000);
+
     it("Initialize Pool", async () => {
         const architect_stake = new anchor.BN(20);
         const builder_stake = new anchor.BN(20);
@@ -34,6 +40,7 @@ describe('Initialize Data Yield Farm', () => {
         const reward_per_block = new anchor.BN(3);
         const pool_cap = new anchor.BN(250000000);
         const penalty = new anchor.BN(3);
+        const poolVault = await mint.getOrCreateAssociatedAccountInfo(admin.publicKey);
         const [pda,bump] = await anchor.web3.PublicKey.findProgramAddress(
             [Buffer.from(anchor.utils.bytes.utf8.encode("Staking"))],
             program.programId
@@ -43,18 +50,21 @@ describe('Initialize Data Yield Farm', () => {
             reward_apy, pool_cap, penalty, reward_per_block,
             {
                 accounts: {
-                    authority: provider.wallet.publicKey,
-                    pda:pda ,
-                    mint: mint,
-                    vault: poolVault,
+                    pda,
+                    authority: admin.publicKey,
+                    mint: mint.publicKey,
+                    vault: poolVault.address,
                     stakingProgram: program.programId,
                     tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
                 },
-                signers: [provider.wallet]
+                signers: [admin]
             });
         let pool = await program.state.fetch();
         const change_vault = await mint.getAccountInfo(pool.vault);
-
-        assert.ok(change_vault.owner, pda);
+        const spda = await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from(anchor.utils.bytes.utf8.encode("Staking"))],
+            program.programId
+        )[0];
+        assert.ok(change_vault.owner, spda);
     }).timeout(90000);
 });
